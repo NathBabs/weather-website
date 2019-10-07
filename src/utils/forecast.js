574b77610abfd89a3f98d019afa9811a2e5639d2
Nathaniel <nathanielbabalola1@gmail.com>
2019-10-07 12:33:47 +0100

    Added new data for weather forecast

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -9,7 +9,7 @@ const foreCast = ({longitude}, {latitude}, callback) => {
             callback(body.error, undefined);
         } else {
             callback(undefined, 
-                body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degrees out. There is a ' + body.currently.precipProbability + ' chance of rain'
+                body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degrees celsius. There is a ' + body.currently.precipProbability + ' chance of rain. <br>The temperature high for today is ' + body.daily.data[0].temperatureHigh  + ' degrees celsius while the temperature low for today is ' + body.daily.data[0].temperatureLow + ' degrees celsius'
             );
         }
     });
